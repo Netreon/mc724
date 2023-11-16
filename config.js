209815329f337a59ff2f56900c8c848b9162b217
Bykedi <122639508+Bykedi@users.noreply.github.com>
2023-11-16 15:27:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "Discord Botun Tokeni",
-    id: "Discord Botun ID'si",
+    token: "MTE2ODU0NTYwMDUxODc2NjczMw.GvkpAZ.7XF1-WyEkQggrWkKgUqH3XdkKg3cFf1FKA89yo",
+    id: "1168545600518766733",
     name: "Exp",
-    premiumroleid: "Discord Premium Rol ID'si",
-    logkanalid: "Discord Log Kanal ID'si"
+    premiumroleid: "1157715425560379553",
+    logkanalid: "1168111286563905587"
 }
